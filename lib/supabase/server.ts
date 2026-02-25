0143a74c3c2a515d141549da7684b16d1a358528
kilic-kts-cyber <kilic-kts@gmx.de>
2026-02-25 11:14:01 +0100

    clean server supabase client

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,22 +1,18 @@
-import { createServerClient } from '@supabase/ssr'
+import { createClient } from '@supabase/supabase-js'
 import { cookies } from 'next/headers'
 
-export function createClient() {
+export function createServerSupabaseClient() {
   const cookieStore = cookies()
 
-  return createServerClient(
+  return createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
-      cookies: {
-        get(name: string) {
-          return cookieStore.get(name)?.value
-        },
-        set(name: string, value: string, options: any) {
-          cookieStore.set({ name, value, ...options })
-        },
-        remove(name: string, options: any) {
-          cookieStore.set({ name, value: '', ...options })
+      global: {
+        headers: {
+          Authorization: `Bearer ${
+            cookieStore.get('sb-access-token')?.value ?? ''
+          }`,
         },
       },
     }
